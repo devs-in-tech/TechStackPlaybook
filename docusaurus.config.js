859def479aee3d18b7bebd2652fa4a8b97281edf
Susmita-Dey <susmitadey475@gmail.com>
2023-07-01 00:37:19 +0530

    deploy to github pages

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,11 +11,11 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://devsintech.netlify.app/',
+  url: 'https://devs-in-tech.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  // baseUrl: '/techstackplaybook/',
-  baseUrl: '/',
+  baseUrl: '/techstackplaybook/',
+  // baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
