7437796ff9c8743a17f31d2c047c02c113d38c0a
Susmita Dey <susmitadey475@gmail.com>
2023-07-01 00:51:15 +0530

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,11 +11,10 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://devs-in-tech.github.io/',
+  url: 'https://techstackplaybook.netlify.app/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/techstackplaybook/',
-  // baseUrl: '/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
